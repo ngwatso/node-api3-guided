2e79881c62ec231dba0b735e49d49e0b0c1a7687
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-03-10 19:48:49 +0100

    wip

--- api/hubs/hubs-middleware.js
+++ api/hubs/hubs-middleware.js
@@ -2,7 +2,9 @@ const Hubs = require('./hubs-model.js');
 
 const checkId = async (req, res, next) => {
   const hub = await Hubs.findById(req.params.id)
-  
+  if (!hub) {
+    
+  }
 }
 
 module.exports = {
