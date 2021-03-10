9ac8d07219f32941e9fa39cb668539932ca7fd09
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-03-10 18:55:47 +0100

    wip

--- api/server.js
+++ api/server.js
@@ -31,7 +31,11 @@ server.get('/', (req, res) => {
 });
 
 server.use((err, req, res, next) => {
-  res.status(500).json()
+  res.status(500).json({
+    message: err.message,
+    stack: err.stack,
+    custom
+  })
 })
 
 module.exports = server;
