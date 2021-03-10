750606c9a54b551519fcf606e2fd093420fb4697
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-03-10 19:49:37 +0100

    wip

--- api/hubs/hubs-middleware.js
+++ api/hubs/hubs-middleware.js
@@ -3,7 +3,12 @@ const Hubs = require('./hubs-model.js');
 const checkId = async (req, res, next) => {
   const hub = await Hubs.findById(req.params.id)
   if (!hub) {
+    res.status(404).json({
+      message: `hub with id ${req.params.id} does not exist`
+    })
+  } else {
     
+    next()
   }
 }
 
