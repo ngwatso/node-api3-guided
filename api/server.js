4dd381917f5e287210200939aad632a675cff0a9
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-03-10 18:58:12 +0100

    wip

--- api/server.js
+++ api/server.js
@@ -33,8 +33,8 @@ server.get('/', (req, res) => {
 
 server.use((err, req, res, next) => {
   res.status(500).json({
-    message: err.message,
-    stack: err.stack,
+    // message: err.message,
+    // stack: err.stack,
     custom: 'something went terrible',
   })
 })
