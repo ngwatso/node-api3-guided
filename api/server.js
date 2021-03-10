080c69180c01bc5a34721afed6b83dea283ff6ad
Gabriel Cabrejas <gabriel@cabrejas.net>
2021-03-10 19:12:50 +0100

    wip

--- api/server.js
+++ api/server.js
@@ -41,7 +41,7 @@ server.use((err, req, res, next) => {
 
 module.exports = server;
 
-const addLambdaHeader = (req, res, next) => {
+const addLambdaHeader = function (req, res, next) {
   res.set('X-Lambda', 'rocks');
   next();
 }
