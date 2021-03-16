0cc384e7fe3245228f299ab675174676c44f9a8b
ngwatso <ngwatson79@yahoo.com>
2021-03-16 11:56:50 -0400

    progress

--- api/hubs/hubs-controller.js
+++ api/hubs/hubs-controller.js
@@ -19,17 +19,23 @@ function getHubs(req, res) {
 		});
 }
 
+// !! this middleware function will always be preceded by a call to validateId()
+// !! validateId ensures that req.hub exists, if the id in the URL is valid
 function getHubById(req, res) {
-	Hubs.findById(req.params.id)
-		.then((hub) => {
-			if (hub) {
-				res.status(200).json(hub);
-			} else {
-				res.status(404).json({ message: 'Hub not found' });
-			}
-		})
-		.catch((err) => {
-			console.log(err);
-			res.status(500).json({ message: 'Error retrieving the hub' });
-		});
+	res.json(req.hub);
 }
+
+// function getHubById(req, res) {
+// 	Hubs.findById(req.params.id)
+// 		.then((hub) => {
+// 			if (hub) {
+// 				res.status(200).json(hub);
+// 			} else {
+// 				res.status(404).json({ message: 'Hub not found' });
+// 			}
+// 		})
+// 		.catch((err) => {
+// 			console.log(err);
+// 			res.status(500).json({ message: 'Error retrieving the hub' });
+// 		});
+// }
